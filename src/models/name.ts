e24ea7ee6c9442060964f2d9a0bb207de3a24ecb
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-07-24 17:43:15 +0530

    Add file collections name contants

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-xspark-qna-devs";
+export const questionCollection = "questions";
+export const answerCollection = "answers";
+export const commentCollection = "comments";
+export const voteCollection = "votes";
+export const questionAttachmentBucket = "question-attachment";
