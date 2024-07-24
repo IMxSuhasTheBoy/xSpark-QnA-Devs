58a081694ca805b6c7539a1626dd549bd573899a
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-07-24 17:41:25 +0530

    Add files appwrite configurations

---
+++ src/models/server/config.ts
@@ -0,0 +1,17 @@
+//? config for connecting appwrite server to db
+import env from "@/app/env";
+import { Client, Databases, Storage, Avatars, Users } from "node-appwrite";
+
+let client = new Client();
+
+client
+  .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+  .setProject(env.appwrite.projectId) // Your project ID
+  .setKey(env.appwrite.apikey); // Your secret API key
+
+const databases = new Databases(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client);
+const users = new Users(client);
+
+export { client, databases, storage, avatars, users };
