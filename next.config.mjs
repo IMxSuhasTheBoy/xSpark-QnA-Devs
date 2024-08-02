18f77ca767f40b8d5c05a6f248b1a7f7469a5cdb
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-08-02 23:50:23 +0530

    Add config for images host remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'cloud.appwrite.io',
+      port: '',
+      // pathname: '/account123/**',
+    },
+  ],
+},};
 
 export default nextConfig;
